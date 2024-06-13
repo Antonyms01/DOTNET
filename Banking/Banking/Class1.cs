3bfa0045cfd94d559dd5fd3fb0bee4066a355f6b
Antonyms01 <sohansarode922@gmail.com>
2024-06-13 15:31:36 +0530

    Banking

---
+++ Banking/Banking/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    class account
+    {
+        string name;
+        int id;
+        int balance;
+
+       public account(string name, int id,int balance)
+        {
+           
+                this.name = name;
+                this.id = id;
+                this.balance = balance;
+           
+        }
+        public void deposit(int amt)
+        {
+            balance = balance + amt;
+            Console.WriteLine(balance);
+        }
+
+        public void withdraw(int amt)
+
+        {
+           if(balance > amt)
+            {
+                balance -= amt;
+                Console.WriteLine(balance);
+            }
+           else
+            {
+                Console.WriteLine("Withdraw amt is greater than balance");
+            }
+           
+          
+        }    
+    }
+
+}
---
+++ Banking/Banking/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            int amt= int.Parse(Console.ReadLine());
+            if (amt >= 10000)
+            {
+                
+                account a1 = new account("Sohan", 1, amt);
+                a1.withdraw(21000);
+        
+             }
+           
+            else
+            {
+                Console.WriteLine("Balance is Low");
+            }
+            
+       }
+    }
+}
+--
